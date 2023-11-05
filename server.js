cf6b4528063d172445f3463cfcdbe2c86df642c2
riyavmt <riyavmt14@gmail.com>
2023-11-05 17:23:51 +0530

    Response from the Server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log("My name is Riya Verma")
+    console.log("My name is Riya Verma");
+    res.end("Welcome to my Node JS Project!");
 });
-server.listen(3000);
+server.listen(4000,() =>{
+    console.log("Server is running")
+});
