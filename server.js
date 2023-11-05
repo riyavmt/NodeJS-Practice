2d413256973b49ff47e7478892e60b2e013b7402
riyavmt <riyavmt14@gmail.com>
2023-11-05 20:47:14 +0530

    Returning Custom responses

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log("My name is Riya Verma");
-    res.end("Welcome to my Node JS Project!");
+    const url = req.url;
+    res.write('<html><head><title>Node JS Application</title></head>');
+    res.write('<body>')
+    if(url==="/home"){
+        res.write("Welcome home ")
+    }
+    else if(url==="/about"){
+        res.write("Welcome to About Us page")
+    }
+    else if(url==="/node"){
+        res.write("Welcome to my node JS Project")
+    }
+    else{
+        res.write("Hello World");
+    } 
+    res.write('</body></html>')
+    res.end();
 });
+
+
 server.listen(4000,() =>{
     console.log("Server is running")
 });
