fa4bfbacec6b2099a7131498dfccaa92fe6c335e
riyavmt <riyavmt14@gmail.com>
2023-11-06 16:04:14 +0530

    Displaying messages in the form

--- server.js
+++ server.js
@@ -37,12 +37,12 @@ const server = http.createServer((req,res)=>{
         });
         
     }
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>')
-    res.write('<body><h2>Hello</h2></body>')
-    res.write('</html>')
-    res.end();
+    // res.setHeader('Content-Type','text/html');
+    // res.write('<html>');
+    // res.write('<head><title>My First Page</title></head>')
+    // res.write('<body><h2>Hello</h2></body>')
+    // res.write('</html>')
+    // res.end();
 });
 
 
